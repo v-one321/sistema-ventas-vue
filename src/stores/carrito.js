cec4f0779cda30bc05c451e1cc2a7954329964d5
v-one321 <vladin321@gmail.com>
2024-07-08 12:59:22 -0400

    Terminamos el almacenamiento en el carrito de compras

--- src/stores/carrito.js
+++ src/stores/carrito.js
@@ -1,22 +1,38 @@
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 export const useCarritoStore = defineStore('carritoMovimiento', () => {
     const carritoCompras = ref([]);
     const carritoVentas = ref([]);
-    const addCarritoCompras = param => {        
-        let objeto = {
-            producto_id: param.id,
-            nombre: param.nombre,
-            codigo: param.codigo,
-            cantidad: 1,
-            precio_unitario: param.precio_compra,
-            precio_total: param.precio_total
-        };
-        carritoCompras.value.push(objeto);
+    const totalCompras = computed(() => carritoCompras.value.reduce((suma, elemento) => { return suma + elemento.precio_total}, 0));
+    const addCarritoCompras = param => {
+        if (!comprobarProducto(param.id)) {
+            let objeto = {
+                producto_id: param.id,
+                nombre: param.nombre,
+                codigo: param.codigo,
+                cantidad: 1,
+                precio_unitario: parseFloat(param.precio_compra),
+                precio_total: parseFloat(param.precio_compra)
+            };
+            carritoCompras.value.push(objeto);
+        }else{
+            const posicion = carritoCompras.value.findIndex(elemento => elemento.producto_id == param.id);
+            carritoCompras.value[posicion].precio_total+=parseFloat(param.precio_compra);
+            carritoCompras.value[posicion].cantidad++;
+        }
+    }
+    const comprobarProducto = (param) => {
+        const existe = carritoCompras.value.some(elemento => elemento.producto_id === param);
+        return existe;
+    }
+    const eliminarCarritoCompra = param => {
+        carritoCompras.value.splice(param, 1);        
     }
     return {
         carritoCompras,
         carritoVentas,
         addCarritoCompras,
+        totalCompras,
+        eliminarCarritoCompra,
     }
 })
--- src/views/movimientos/Compras-formulario.vue
+++ src/views/movimientos/Compras-formulario.vue
@@ -11,8 +11,10 @@
                         <div class="row gy-3">
                             <div class="col-12">
                                 <label for="proveedor" class="form-label fw-bold text-muted">Proveedor</label>
-                                <select name="proveedor" id="proveedor" class="form-select">
+                                <select name="proveedor" id="proveedor" class="form-select" v-model="proveedor">
                                     <option value="">Seleccione</option>
+                                    <option :value="item.id" v-for="item in proveedores" :key="item.id">{{ item.nombre
+                                        }} {{ item.apellido }}</option>
                                 </select>
                             </div>
                             <div class="col-12">
@@ -29,15 +31,21 @@
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            <tr v-for="(item, indice) in carritoCompras" :key="indice">
+                                            <tr v-for="(item, indice) in useStore.carritoCompras" :key="indice">
                                                 <td>{{ item.nombre }}</td>
                                                 <td>{{ item.codigo }}</td>
                                                 <td>{{ item.precio_unitario }}</td>
                                                 <td>{{ item.cantidad }}</td>
-                                                <td>{{ item.precio_unitario }}</td>
-                                                <td></td>
+                                                <td>{{ item.precio_total }}</td>
+                                                <td><button type="button" class="btn btn-danger btn-sm"
+                                                        @click="useStore.eliminarCarritoCompra(indice)"><i
+                                                            class="fas fa-trash"></i></button></td>
                                             </tr>
                                         </tbody>
+                                        <tfoot>
+                                            <th colspan="4" class="text-end fs-4 fw-bold">TOTAL</th>
+                                            <th class="text-end fs-4">{{ useStore.totalCompras }}Bs</th>
+                                        </tfoot>
                                     </table>
                                 </div>
                             </div>
@@ -45,15 +53,45 @@
                     </div>
                 </div>
             </div>
+            <div class="col-12 text-center">
+                <button class="btn btn-danger btn-lg me-2"><i class="fas fa-reply me-2"></i>Volver</button>
+                <button class="btn btn-success btn-lg ms-2" @click="guardar()">Guardar<i class="fas fa-save ms-2"></i></button>
+            </div>
         </div>
     </div>
 </template>
 <script setup>
 import ProductosActivosVue from '@/components/Productos-activos.vue';
+import { store } from '@/services/comprasService';
+import { proveedoresActivos } from '@/services/proveedoresService';
 import { useCarritoStore } from '@/stores/carrito';
-const { carritoCompras } = useCarritoStore();
-
+import { ref, onMounted } from 'vue'
+const useStore = useCarritoStore();
+const proveedores = ref([]);
+const proveedor = ref('');
+const listar = async () => {
+    try {
+        const { data } = await proveedoresActivos();
+        proveedores.value = data.datos;
+    } catch (error) {
+        console.log(error);
+    }
+}
+onMounted(() => {
+    listar();
+})
+const guardar = async () => {
+    try {
+        let objeto = {
+            total: useStore.totalCompras,
+            proveedor_id: proveedor.value,
+            detalle: useStore.carritoCompras
+        }
+        const { data } = await store(objeto);
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
 </script>
-<style>
-    
-</style>
+<style></style>
